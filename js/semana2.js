a08096042f0dcca6a20189db8017f3fd8d085ab1
jrojasr98 <jrojasr.1998@gmail.com>
2022-10-06 16:40:11 +0000

    actualizacion
    Clases

---
+++ js/semana2.js
@@ -0,0 +1,12 @@
+/*
+ * FUNDAMENTOS JS
+ * 24/09/2022
+ */
+
+ "use strict"
+   
+ var miVariable = 5;
+ var saludo = "Hola Mundo Desde JS";
+ var miBool = true; 
+
+ console.log(saludo, miVariable);
